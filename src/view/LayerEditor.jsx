81371944a9320bfb28d5cfee025902e6630c49ea
Mikael Lindqvist <li.mikael@gmail.com>
2020-01-18 19:24:22 +0800

    sequence grid

--- src/view/LayerEditor.jsx
+++ src/view/LayerEditor.jsx
@@ -5,10 +5,48 @@ export default class LayerEditor {
 		this.props.layer.instrument.play(index);
 	};
 
+	onKeyDown=(e)=>{
+		if (parseInt(e.key)) {
+			this.props.layer.instrument.play(parseInt(e.key)-1);
+		}
+	}
+
+	componentDidMount() {
+		document.addEventListener("keydown",this.onKeyDown);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener("keydown",this.onKeyDown);
+	}
+
+	renderRow(label, index) {
+		return (
+			<tr>
+				<th>{label}</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		);
+	}
+
 	render() {
 		let buttons=this.props.layer.instrument.getSoundLabels().map((label,index)=>{
 			return (
-				<button type="button" class="btn btn-primary btn-lg mb-3 mr-3"
+				<button type="button" class="btn btn-primary btn-lg mb-2 mr-2"
 						style={{width: '6em', height: '6em'}}
 						onMouseDown={this.onInstrumentButtonPress.bind(this,index)}>
 					{label}
@@ -25,7 +63,31 @@ export default class LayerEditor {
 					</button>
         		</div>
 				<div className="card-body">
-					{buttons}
+					<ul class="nav nav-tabs mb-3">
+						<li class="nav-item">
+							<a class="nav-link active" data-toggle="tab" href="#pad">Pad</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" data-toggle="tab" href="#sequence">Sequence</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" data-toggle="tab" href="#layer">Layer</a>
+						</li>
+					</ul>
+
+					<div class="tab-content">
+						<div class="tab-pane fade active show" id="pad">
+							{buttons}
+						</div>
+						<div class="tab-pane fade" id="sequence">
+							<table class="sequence-table">
+								{this.props.layer.instrument.getSoundLabels().map(this.renderRow)}
+							</table>
+						</div>
+						<div class="tab-pane fade" id="layer">
+							<button type="button" class="btn btn-danger">Delete Layer</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
