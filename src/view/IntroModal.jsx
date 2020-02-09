0c79024d80ad22e26e80a8cd45f7a9d1f926fe3a
Mikael Lindqvist <li.mikael@gmail.com>
2020-02-09 18:10:01 +0800

    intro text

--- src/view/IntroModal.jsx
+++ src/view/IntroModal.jsx
@@ -21,61 +21,60 @@ export default class IntroModal extends Component {
 							</button>
 						</div>
 						<div className="modal-body">
-							<h2>Thanks!</h2>
+							<h2>Need your honest feedback!</h2>
 							<p>
-								Thanks for trying out my app! Please note that this
-								is a <b>super early prototype</b>. To understand the basic idea,
-								please see this video:
-							</p>
-							<p class="ml-4">
-								<a href="https://www.facebook.com/li.mikael/videos/10157084270912339/"
+								This is a super early prototype of my app. It is a tool for 
+								musicians to play around with rhythm, baselines and chords in a 
+								live setting. Here is a <a 
+										href="https://www.facebook.com/li.mikael/videos/10157084270912339/"
 										target="_blank" class="text-info">
-									https://www.facebook.com/li.mikael/videos/10157084270912339/
-								</a>
+									video
+								</a> to explain the basic idea.
+								At the moment, the prototype is a bit messy and is not really
+								straightforward to use. This is where you come in!
 							</p>
+							<h3>Just tell me this</h3>
+							<ol>
+								<li>Do you get the basic idea?</li>
+								<li>Are you able to get the app to make any sound?</li>
+								<li>How can the app be improved?</li>
+							</ol>
 							<p>
-								There are also a few other things I would like to point out before
-								you try it.
+								Any feature suggestions and comments are welcome!
 							</p>
+							<h3>Things to keep in mind</h3>
 							<ul>
 								<li>
-									Things will change and might not work as expected. Please don't get 
-									attached to anything and don't get upset if it changes. Also, please
-									promise that you will not lose hope, and give it another try later if
-									there is something that isn't up to standard at this point.
+									<b>There is no documentation.</b> Just click around and explore,
+									don’t be afraid to break it! If something seems broken, tell me about it!
 								</li>
 								<li>
-									There is no documentation. If there is something that you
-									don't understand, it is because I haven't yet thought through
-									and tested the user interface. There is nothing wrong with your
-									ability to understand, there is something wrong with the app.
+									<b>Not a mobile app yet.</b> Even though this is going to be a mobile 
+									app eventually, at the moment it doesn't really work on mobiles 
+									at all. This is because this is just a very quick technology
+									test at the moment.
 								</li>
 								<li>
-									Even though this is going to be a mobile app eventually,
-									at the moment it doesn't really work on mobiles at all. This is
-									because this is just a very quick technology test at the moment.
+									<b>The built-in instruments sound like shit.</b> Or well, surprisingly 
+									they can actually be used to create music, but they sound 
+									nothing like they eventually will sound. 
 								</li>
 								<li>
-									The built in instruments sound like shit. Or well, surprisingly
-									they can actually be used to create music, but they sound nothing
-									like they eventually will sound. Please have patience!
+									<b>Tracks are not downloadable for now.</b> At the moment, the tracks
+									created get stored locally in your browser. This will also change,
+									I'm envisioning a system whereby you can share the songs/tracks
+									across several different devices, export them for usage in other
+									programs, etc, etc. But nothing of this is in place yet.
 								</li>
 								<li>
-									At the moment, the songs created gets stored locally in your browser.
-									This will also change, I'm envisioning a system whereby you can share
-									the songs across several different devices, export them for usage in
-									other programs, etc, etc. But nothing of this is in place yet. Also,
-									the internal format that the app uses to save songs will probably change,
-									which means that things you create now might not work in future versions
-									of the app.
+									<b>Features will change fast.</b> If you potentially start using
+									the app, please don't get attached to anything and don't get
+									upset if it changes. Also, promise that you will not lose hope, 
+									and give it another try later if there is something that isn't
+									up to standard at this point.
 								</li>
 							</ul>
-							<p>
-								That being said, please go ahead and test it! And again, please don't
-								be too quick to pass judgement if there is something that is confusing
-								or doesn't work. In this case, please reach out to me! Would love
-								to have a chat!
-							</p>
+							<h3>Reach me via</h3>
 							<p class="ml-4">
 								Email:
 								<a href="mailto:li.mikael@gmail.com" target="_blank" class="text-info ml-2">
@@ -87,6 +86,12 @@ export default class IntroModal extends Component {
 									https://www.facebook.com/li.mikael
 								</a>
 							</p>
+							<p>Or put your comments as a reply to <a 
+									href="https://www.facebook.com/li.mikael/videos/10157084270912339/"
+									target="_blank" class="text-info">
+								this</a> facebook post.
+							</p>
+							<p>Thanks for trying out my app!</p>
 							<p>// Micke</p>
 						</div>
 						<div className="modal-footer">
