3c3993035c02cfdbbb5dabeb34fe19213dcb5143
Mikael Lindqvist <li.mikael@gmail.com>
2020-02-15 15:45:23 +0800

    other test

---
+++ test/reconcile-test-2.js
@@ -0,0 +1,22 @@
+let data=[{
+	key: 1,
+	label: "hello"
+},{
+	key: 2
+	label: "world"
+}];
+
+class MyClass {
+
+}
+
+array=new ReconcileArray(MyClass);
+
+array.setData(data);
+
+let data=[{
+	key: 1,
+	label: "hello"
+}];
+
+array.setData(data);
