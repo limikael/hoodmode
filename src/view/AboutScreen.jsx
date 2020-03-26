d86eda2d9b8eb8e745ccfb5fb8d137372c56e71f
Mikael Lindqvist <li.mikael@gmail.com>
2020-03-26 18:03:09 +0800

    test abc

--- src/view/AboutScreen.jsx
+++ src/view/AboutScreen.jsx
@@ -9,7 +9,7 @@ export default class AboutScreen extends Component {
 				<div class="box border border-dark bg-background about-screen">
 					<div class="pane-header text-secondary bg-dark ">ABOUT</div>
 					<b>Hoodmode</b><br/><br/>
-					Version {this.context.getAppVersion()}<br/><br/>
+					Version: {this.context.getAppVersion()}<br/><br/>
 					Enjoy! Please let me know of any bugs you find!
 				</div>
 			</div>
