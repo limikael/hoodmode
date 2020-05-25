2ef875d3b6c92efc0654c40288f53743a1c83f7a
Dea <dea@Deas-MacBook-Air.local>
2020-05-25 17:01:59 +0800

    ios build

--- .travis/install_ios.sh
+++ .travis/install_ios.sh
@@ -9,3 +9,12 @@ security unlock-keychain -p nosecret build.keychain
 sudo npm install -g cordova
 npm install
 bundle install
+
+ALTOOL='/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool'
+if [ ! -f "$ALTOOL" ]; then
+	ALDIR=`dirname "$ALTOOL"`
+	mkdir -p "$ALDIR"
+
+	printf '#!/usr/bin/env bash\nxcrun altool $@\n' > "$ALTOOL"
+	chmod 755 "$ALTOOL"
+fi
