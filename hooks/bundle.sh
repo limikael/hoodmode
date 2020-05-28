c362e69eee164bddcedd1d22f39c94802306b322
Mikael Lindqvist <li.mikael@gmail.com>
2020-05-28 09:50:17 +0800

    0.0.22

--- hooks/bundle.sh
+++ hooks/bundle.sh
@@ -1,3 +1,6 @@
+echo "Updating cordova version in config.xml"
+./node_modules/.bin/cordova-set-version
+
 echo "Bundling javascript (release)"
 #./node_modules/.bin/browserify -t babelify --debug src/index.jsx -o www/index.js
 ./node_modules/.bin/browserify -t babelify -t uglifyify src/index.jsx -o www/index.js
